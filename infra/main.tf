797c2b167ad0d21793dcbaa9ca42b6c7d2164275
rubenilsonbj <rubenilsonbj@gmail.com>
2026-01-17 09:27:14 -0300

    initial commit

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,8 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
